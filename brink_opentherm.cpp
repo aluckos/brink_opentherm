38494c15bfa0c7bac92a13223911b0725bf2e27b
aluckos <59487836+aluckos@users.noreply.github.com>
2025-12-03 13:12:56 +0100

    Update brink_opentherm.cpp

--- brink_opentherm.cpp
+++ brink_opentherm.cpp
@@ -1,4 +1,3 @@
-#include "brink_opentherm.h"
-
-// Plik jest minimalny, cała logika jest w loop() i setup() w header
-// Możesz tu dodać np. callbacki do liczników, alarmów lub harmonogramów.
+void setup() override {
+    App.register_component(this);
+}
